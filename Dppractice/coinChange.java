122e93430435c2c18b33036ce71c56078251707b
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-03-20 09:02:51 +0530

    coinChange is added

--- Dppractice/coinChange.java
+++ Dppractice/coinChange.java
@@ -1,31 +1,44 @@
 package Dppractice;
 
+import java.util.Arrays;
+
 public class coinChange {
-    public static int coinChange2(int[] coins, int amount) {
-
-        int dp[][] = new int[coins.length ][amount + 1];
-        for (int i = 0; i <= amount; i++) {
-            if(i % coins[0] ==0)
-            dp[0][i] = 1;
-
-        }
-        for (int ind = 1; ind < coins.length; ind++) {
-            for (int target = 0; target <= amount; target++) {
-                int notTake = dp[ind - 1][target];
-                int take = 0;
-                if (coins[ind] <= target)   take = dp[ind][target - coins[ind]];
-                
-                dp[ind][target] = take + notTake;
-            }
+    public static int coinChange2(int[] coins, int amount, int[][] dp, int index) {
+        if (index == 0) {
+            if (amount % coins[0] == 0)
+                return amount / coins[0];
+            return (int) 1e9;
         }
 
-        return dp[coins.length-1][amount];
+        if (dp[index][amount] != -1)
+            return dp[index][amount];
+
+        int notTake = 0 + coinChange2(coins, amount, dp, index - 1);
+
+        int take = Integer.MAX_VALUE;
+        if (coins[index] <= amount) {
+            take = 1 + coinChange2(coins, amount - coins[index], dp, index);
+        }
+
+        return dp[index][amount] = Math.min(take ,notTake);
+
+    }
+
+    public static int change(int[] coins, int amount) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        for (int row[] : dp) {
+            Arrays.fill(row, -1);
+        }
+        int ans = coinChange2(coins, amount, dp, coins.length - 1);
+        if (ans >= (int) 1e9)
+            return -1;
+        return ans;
+
     }
 
     public static void main(String[] args) {
-       int[] coins = {1,2,5};
-       int amount = 11;
-System.out.println(coinChange2(coins, amount));
-       
+        int[] coins = { 1, 2, 5 };
+        int amount = 11;
+System.out.println(change(coins, amount));
     }
 }
