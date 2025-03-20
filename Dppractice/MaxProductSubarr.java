72e49a48636950e2bb2c6326611549019336a9e3
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-03-20 20:05:41 +0530

    MaxPrdocutSubarray is Added

--- Dppractice/MaxProductSubarr.java
+++ Dppractice/MaxProductSubarr.java
@@ -1,14 +1,28 @@
 package Dppractice;
 
 public class MaxProductSubarr {
+    public static int maxProduct(int[] nums) {
+        int suffix = 1;
+        int prefix = 1;
+        int ans = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (prefix == 0)
+                prefix = 1;
+            if (suffix == 0)
+                suffix = 1;
 
-    public static int Halper(){
+            prefix *= nums[i];
+            suffix *= nums[n - i - 1];
 
+            ans = Math.max(ans, Math.max(prefix, suffix));
+        }
+
+        return ans;
     }
-    public int maxProduct(int[] nums) {
-        return 
-    }
+
     public static void main(String[] args) {
-        
+        int[] nums = { -2, 0, -1 };
+        System.out.println(maxProduct(nums));
     }
 }
